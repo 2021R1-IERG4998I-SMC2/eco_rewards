de9062726384df0c19e38a67493d8e6a85932ac7
Cliff Lau <cliff@clifflau.hk>
2021-12-05 03:21:01 +0800

    Fix provider leftover.

--- lib/src/screens/home/tabs/explore_tab.dart
+++ lib/src/screens/home/tabs/explore_tab.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'package:eco_rewards/src/components/home/app_bar.dart';
 
